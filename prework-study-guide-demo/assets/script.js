f16766af7faa79508817ed5d61c7434061b90dbd
Shaontae Robinson <shaontae3@gmail.com>
2023-07-12 09:48:59 -0400

    added arrays to study guide

--- prework-study-guide-demo/assets/script.js
+++ prework-study-guide-demo/assets/script.js
@@ -7,7 +7,13 @@ if (topic === 'HTML') {
 } else if (topic === 'Git') {
   console.log("Let's study Git!");
 } else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else {
+  console.log("Let's study Javascript!");
+}
+else {
   console.log('Please try again!');
 }
+var shapes = ["triangle", "square", "pentagon", "circle"];
+console.log(shapes[0]);
+console.log(shapes[1]);
+console.log(shapes[2]);
+console.log(shapes[3]);
