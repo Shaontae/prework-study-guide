7decf777dbecdfdbafd50b5dd6985fcaf0ab76ac
Shaontae Robinson <shaontae3@gmail.com>
2023-07-13 12:35:31 -0400

    added javascript file

--- prework-study-guide-demo/assets/script.js
+++ prework-study-guide-demo/assets/script.js
@@ -1,28 +1,27 @@
-/*var topic = "Array";
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study Javascript!");
+function listTopics() {
+ for (var x = 0; x < topics.length; x++) {
+   console.log(topics[x]);
+ }
 }
-  else if (topic === 'Array') {
-  console.log("Let's study Array!");
+
+function selectTopic() {
+ if (randomTopic === 'HTML') {
+   console.log("Let's study HTML!");
+ } else if (randomTopic === 'CSS') {
+   console.log("Let's study CSS!");
+ } else if (randomTopic === 'Git') {
+   console.log("Let's study Git!");
+ } else if (randomTopic === 'JavaScript') {
+   console.log("Let's study JavaScript!");
+ } else {
+   console.log('Please try again!');
+ }
 }
-else {
-  console.log('Please try again!');
-}
-var topics = ["HTML", "Css", "Git", "Javascript","Array"];
-console.log(topics[0]);
-console.log(topics[1]);
-console.log(topics[2]);
-console.log(topics[3]);
-console.log(topics[4]);
-*/
-var shapes = ["HTML", "Css", "Git", "Javascript", "Array"];
-for(var x = 0; x < shapes.length; x++) {
-console.log(shapes[x]);
-}
+
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
